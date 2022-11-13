594cb3279f45b0fcf47001798642ebb607f64a8c
ruru <coco002197@gmail.com>
2022-11-13 15:12:18 +0800

    refactor: response of homepage html

--- lesson-4/routes/home.js
+++ lesson-4/routes/home.js
@@ -4,14 +4,25 @@ const users = require('../db/users.js')
 
 router.get('/', (request, response, next) => {
     response.status(200).send(`
-        <h2>Home Page</h2>
-        <nav>
-            <a href="http://localhost:3000/">home</a> |
-            <a href="http://localhost:3000/user">user</a>
-        </nav>
-        <p>
-            目前共有 ${users.length} 名用戶
-        </p>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Document</title>
+
+            </head>
+            <body>
+                <h2>Home Page</h2>
+                <nav>
+                    <a href="http://localhost:3000/">home</a> |
+                    <a href="http://localhost:3000/user">user</a>
+                </nav>
+                <p>
+                    目前共有 ${users.length} 名用戶
+                </p>
+            </body>
+        </html>
     `)
 })
 
